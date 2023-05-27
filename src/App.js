30326b2d0757fbafcdbc814b9462b3611a5545e8
Sebastian Szeszko <sebastian.szeszko@primigenius.pl>
2023-05-27 20:58:19 +0200

    fix fibbonaci sequence starting from element zero instead of first

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ function App() {
     }
 
     function fibbonaci(a) {
-        if (a == 0) return 0;
-        if (a == 1) return 1;
+        if (a == 1) return 0;
+        if (a == 2) return 1;
         return fibbonaci(a - 1) + fibbonaci(a - 2);
     }
 
